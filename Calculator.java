e222a1311e06e2be40a8aa67ab879e46c245ce84
최영환/네무소프트 <choiyh44@gmail.com>
2015-06-04 15:18:49 +0900

    fixed#1 스켈레톤 작성

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+
+public Class Calculator {
+	public static void main(String[] args) throws Exception {
+		int a = 1;
+		int b = 2;
+		
+		new Calculator().add(a, b);
+	}
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
